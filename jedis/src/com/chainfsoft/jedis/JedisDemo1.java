6982c66fbbd3cc43b77042eedcf040d70ae07cf4
fengjifei0708 <1109587205@qq.com>
2018-07-10 18:36:33 +0800

    第二个a22.php

---
+++ jedis/src/com/chainfsoft/jedis/JedisDemo1.java
@@ -0,0 +1,27 @@
+package com.chainfsoft.jedis;
+
+
+
+import org.junit.Test;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPoolConfig;
+
+public class JedisDemo1 {
+	@Test
+	/**
+	 *一般方式链接
+	 */
+  public void demo1() {
+	  //1.设置IP地址和端口
+	  Jedis jedis = new Jedis("127.0.0.1",6379);
+	  //2.保存数据
+	  jedis.set("name", "fengjifei");
+	  //3.获取数据
+	  String value = jedis.get("name");
+	  System.out.println(value);
+	  //4.释放资源
+	  jedis.close();
+  }
+	
+}
